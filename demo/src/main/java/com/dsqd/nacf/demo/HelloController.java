5f5985182bae300e692b3a9e6a4957f3aae021a4
lej0718-dsq <lej0718@gmail.com>
2025-10-12 21:44:42 +0900

    1차 개발항목

---
+++ demo/src/main/java/com/dsqd/nacf/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.dsqd.nacf.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello vscode";
+    }
+}
